c780e55621372196706fbcf544feabe034e19328
Akshay <blitz122333@gmail.com>
2025-03-16 00:43:04 +0530

    request body

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 
 # http://127.0.0.1:8000/blog?limit=104
 @app.get('/blog')
-def index(limit, published:bool):
+def index(limit=10, published:bool=True):
     # return published
     if published:
         # http://127.0.0.1:8000/blog?limit=104&published=true
@@ -31,3 +33,14 @@ def show(id:int):
 @app.get('/blog/{id}/comments')
 def comments(id):
     return {"data": {'1', '2'}}
+
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data':f'blog post has been created with title {blog.title}'}
