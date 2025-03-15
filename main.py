751a7a46a60a18b6b35026e0f1b8655ae69a667f
Akshay <blitz122333@gmail.com>
2025-03-16 00:29:30 +0530

    path and query parameters

--- main.py
+++ main.py
@@ -2,10 +2,32 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Akshay'}}
 
-@app.get('/about')
-def about():
-    return 'This is an About page.'
+# http://127.0.0.1:8000/blog?limit=104
+@app.get('/blog')
+def index(limit, published:bool):
+    # return published
+    if published:
+        # http://127.0.0.1:8000/blog?limit=104&published=true
+        return {'data': f'{limit} published blogs from db.'}
+    else:
+        # http://127.0.0.1:8000/blog?limit=104&published=false
+        return {'data': f'{limit} blogs from db.'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+#Keep the dynamic routing below the static ones
+@app.get('/blog/{id}')
+def show(id:int):
+    #fetch blog with id = id
+    return {'data': id}
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {"data": {'1', '2'}}
